d1947b76c058d793f9d51f667b71bab5c26d75d9
dashIink <71524804+dashIink@users.noreply.github.com>
2023-03-07 13:51:57 -0500

    Added Board as 9x9 array

---
+++ solver.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include <unistd.h>
+#include <limits.h>
+#include <dirent.h>
+#include <sys/wait.h>
+#include <pthread.h>
+
+
+int board[9][9] = {
+	{5,3,4,6,7,8,9,1,2},
+	{6,7,2,1,9,5,3,4,8},
+	{1,9,8,3,4,2,5,6,7},
+	{8,5,9,7,6,1,4,2,3},
+	{4,2,6,8,5,3,7,9,1},
+	{7,1,3,9,2,4,8,5,6},
+	{9,6,1,5,3,7,2,8,4},
+	{2,8,7,4,1,9,6,3,5},
+	{3,4,5,2,8,6,1,7,9}};
+
+
+int main(){
+		
+}
