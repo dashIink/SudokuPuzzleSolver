a8ada6551b24805f228e31c92e7c156a1fd61a6e
dashIink <71524804+dashIink@users.noreply.github.com>
2023-03-09 19:55:21 -0500

    Added Checks
    Checks all rows, columns and squares using 3 threads. I know it says 9 but that is just ridiculous.

--- solver.c
+++ solver.c
@@ -20,7 +20,80 @@ int board[9][9] = {
 	{2,8,7,4,1,9,6,3,5},
 	{3,4,5,2,8,6,1,7,9}};
 
+bool columnCheck = true;
+bool rowCheck = true;
+bool squareCheck = true;
+
+
+void *checkRows(void *){
+    int check[9] = {0,0,0,0,0,0,0,0,0};
+    for(int i = 0; i < 9; i++){
+        for(int x = 0; x < 9; x++){
+            
+            check[board[i][x]-1] += 1;
+            
+            if (check[board[i][x]-1] > 1){
+                rowCheck = false;
+            }
+        }
+        for(int x = 0; x < 9; x++){ 
+            check[x] = 0;
+        }
+    }
+    
+}
+void *checkColumns(void *){
+    int check[9] = {0,0,0,0,0,0,0,0,0};
+    for(int i = 0; i < 9; i++){
+        for(int x = 0; x < 9; x++){
+            
+            check[board[x][i]-1] += 1;
+            
+            if (check[board[i][x]-1] > 1){
+                columnCheck = false;
+            }
+        }
+        for(int x = 0; x < 9; x++){ 
+            check[x] = 0;
+        }
+    }
+}
+
+void *checkSquares(void *){
+    int check[9] = {0,0,0,0,0,0,0,0,0};
+    for(int i = 0; i < 9; i+=3){
+        for(int x = 0; x < 9; x+=3){
+            for(int j = i; j < i+3; j++){
+                for (int h = x; h < x+3; h++){
+                    check[board[j][h]-1] += 1;
+                    if (check[board[j][h]-1] > 1){
+                        squareCheck = false;
+                    }
+                }
+            }
+        for(int x = 0; x < 9; x++){ 
+            check[x] = 0;
+        }
+        }
+        
+    }
+}
 
 int main(){
+		pthread_t id[3];
+		//int arg = 1;
+		pthread_create(&id[0], NULL, *checkRows, NULL);
+		pthread_create(&id[1], NULL, *checkColumns, NULL);
+		pthread_create(&id[2], NULL, *checkSquares, NULL);
+		
+		pthread_join(id[0], NULL);
+		pthread_join(id[1], NULL);
+		pthread_join(id[2], NULL);
+		if (rowCheck && columnCheck && squareCheck){
+		    printf("It is a correct sudoku Puzzle!");
+		}
+		else{
+		    printf("This puzzle has errors!");
+		}
 		
 }
