f7fc04184337963dd27496a2544dc774220be153
dashIink <71524804+dashIink@users.noreply.github.com>
2023-03-13 21:59:46 -0400

    Update solver.c

--- solver.c
+++ solver.c
@@ -1,3 +1,9 @@
+//Tal Marianovski [100700316]
+//Thomas Menegotti [100750648]
+//Liam Rea [100743012]
+//Hishan Sayanthan [100786649]
+
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
